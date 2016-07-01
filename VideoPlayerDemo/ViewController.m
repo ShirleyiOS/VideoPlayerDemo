38d124aae785318ddb7e440473053bb7cd61f01f
ShirleyiOS <shuang.wang0909@gmail.com>
2016-07-01 22:22:31 +0800

    just implement video play function

--- VideoPlayerDemo/ViewController.m
+++ VideoPlayerDemo/ViewController.m
@@ -7,21 +7,47 @@
 //
 
 #import "ViewController.h"
-
+#import <AVFoundation/AVFoundation.h>
 @interface ViewController ()
-
+@property (nonatomic, strong) AVPlayer *avPlayer;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+    CGFloat ratio = 9.0 / 16.0;
+    UIView *playView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, screenWidth, screenWidth * ratio)];
+    playView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:playView];
+    
+    AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:[NSURL URLWithString:@"http://45.113.121.198/bangbang/2016-06-29/bangbang_20160629015256.mp4"]];
+    [playerItem addObserver:self forKeyPath:@"loadedTimeRanges" options:NSKeyValueObservingOptionNew context:nil];
+    [playerItem addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    self.avPlayer = [AVPlayer playerWithPlayerItem:playerItem];
+    
+    AVPlayerLayer *playerLayer = [AVPlayerLayer playerLayerWithPlayer:self.avPlayer];
+  //  playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+   // playerLayer.contentsScale = [UIScreen mainScreen].scale;
+    playerLayer.frame = playView.bounds;
+    [playView.layer addSublayer:playerLayer];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context
+{
+    AVPlayerItem *playerItem = (AVPlayerItem *)object;
+    
+    if ([keyPath isEqualToString:@"loadedTimeRanges"]){
+        
+    }else if ([keyPath isEqualToString:@"status"]){
+        if (playerItem.status == AVPlayerItemStatusReadyToPlay){
+            NSLog(@"playerItem is ready");
+            [self.avPlayer play];
+        } else{
+            NSLog(@"load break");
+        }
+    }
 }
 
 @end
