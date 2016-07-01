1d347c475f94fadebc1afab267dcd92ba7e0abac
ShirleyiOS <shuang.wang0909@gmail.com>
2016-07-01 21:39:34 +0800

    Initial Commit

---
+++ VideoPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoPlayerDemo
+//
+//  Created by 王爽 on 16/7/1.
+//  Copyright © 2016年 王爽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoPlayerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VideoPlayerDemo
+//
+//  Created by 王爽 on 16/7/1.
+//  Copyright © 2016年 王爽. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
